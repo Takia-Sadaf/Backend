8a25349d0637e7606f12976db929cecfd15c292a
Takia Sadaf <2022-1-60-255@std.ewubd.edu>
2024-09-16 02:04:37 +0600

    register user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,46 @@
 import { asyncHandler } from "../utils/asynchandler.js";
-const registerUser=asyncHandler(async,(req,res)=>{
-res.status(200).json({
-    message:"ok"
+import { apiError } from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
+import { cloudinaryUpload } from "../utils/cloudinary.js";
+import { apiResponse } from "../utils/apiResponse.js";
+const registerUser=asyncHandler(async(req,res)=>{
+
+
+const{username,email,password}=req.body
+
+if ([username,email,password].some((field)=>field?.trim()==="" 
+)){
+    throw new apiError(400,"all fields are required")
+}
+const existedUser=User.findOne({email})
+if(existedUser){
+    throw new apiError(409,"user with this email already existed")
+}
+const imageLocalpath=req.files?.Image[0]?.path;
+if (!imageLocalpath){
+    throw new apiErrorError(400,"image is required");
+    
+}
+const image=await cloudinaryUpload(imageLocalpath)
+if (!image) {
+    throw new apiErrorError(400,"image is required");
+    
+}
+const user=await User.create({
+    username,
+    image:image.url,
+    email,
+    password
 })
+const createdUser=await User.findById(user._id).select("-password-refreshToken")
+if(!createdUser){
+    throw new apiError(500,"something went wrong");
+    
+}
+return res.status(201).json(
+    new apiResponse(200,createdUser,"User registered successfully >_<")
+)
 }
 )
+
 export{registerUser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-
+import { upload } from "../middlewares/multer.js";
 const router=router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name:'Image',
+            maxCount:1
+        }
+
+
+    ]),
+ 
+ registerUser)
 router.route("/login").post(login)
 export default router
